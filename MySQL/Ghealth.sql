f21f5ef97d23a491e40e28778d0a8d5dffbc5ce4
Amir Shtaiman <Amir Shtaiman>
2016-05-16 20:15:40 +0300

    SQL Tables

---
+++ MySQL/Ghealth.sql
@@ -0,0 +1,162 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win32 (AMD64)
+--
+-- Host: localhost    Database: ghealth
+-- ------------------------------------------------------
+-- Server version	5.7.9-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clinic`
+--
+
+DROP TABLE IF EXISTS `clinic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clinic` (
+  `cID` int(11) NOT NULL,
+  `cName` varchar(45) DEFAULT NULL,
+  `cLocation` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clinic`
+--
+
+LOCK TABLES `clinic` WRITE;
+/*!40000 ALTER TABLE `clinic` DISABLE KEYS */;
+INSERT INTO `clinic` VALUES (1,'Asuta','Haifa'),(2,'Killers','Tel-Aviv');
+/*!40000 ALTER TABLE `clinic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
+  `ptID` varchar(45) NOT NULL,
+  `ptFirstName` varchar(45) DEFAULT NULL,
+  `ptLastName` varchar(45) DEFAULT NULL,
+  `ptEmail` varchar(45) DEFAULT NULL,
+  `ptPhone` varchar(45) DEFAULT NULL,
+  `ptPrivateClinic` varchar(45) DEFAULT NULL,
+  `ptDoctorID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ptID`),
+  KEY `PersonalDoctorID_idx` (`ptDoctorID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES ('200113','Ori','Arel','temp@gmail.com','12345','Klalit',1);
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personaldoctor`
+--
+
+DROP TABLE IF EXISTS `personaldoctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personaldoctor` (
+  `PersonalDoctorID` int(11) NOT NULL,
+  `PersonalDoctorName` varchar(45) DEFAULT NULL,
+  `PersonalDoctorEmail` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`PersonalDoctorID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personaldoctor`
+--
+
+LOCK TABLES `personaldoctor` WRITE;
+/*!40000 ALTER TABLE `personaldoctor` DISABLE KEYS */;
+INSERT INTO `personaldoctor` VALUES (1,'Muhamad Ali','blabla@gmail.com'),(2,'Yaser Arafat','asdfas@gmail.com');
+/*!40000 ALTER TABLE `personaldoctor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `privateclniic`
+--
+
+DROP TABLE IF EXISTS `privateclniic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `privateclniic` (
+  `PrivateClinicName` varchar(45) NOT NULL,
+  `PrivateClinicEmail` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`PrivateClinicName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `privateclniic`
+--
+
+LOCK TABLES `privateclniic` WRITE;
+/*!40000 ALTER TABLE `privateclniic` DISABLE KEYS */;
+INSERT INTO `privateclniic` VALUES ('Klalit','Klalit@gmail.com'),('Maccabi','Maccabi@gmail.com');
+/*!40000 ALTER TABLE `privateclniic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `uID` varchar(45) NOT NULL,
+  `uPassword` varchar(45) DEFAULT NULL,
+  `uFirstName` varchar(45) DEFAULT NULL,
+  `uLastName` varchar(45) DEFAULT NULL,
+  `uEmail` varchar(45) DEFAULT NULL,
+  `ucID` int(11) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`uID`),
+  KEY `cID_idx` (`ucID`),
+  CONSTRAINT `cID` FOREIGN KEY (`ucID`) REFERENCES `clinic` (`cID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('5000','1234','Moshe','Pinto','Pinto@gmail.com',1,'DOCTOR');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-16 20:13:24
