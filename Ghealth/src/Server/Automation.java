236467caafb03ce9a1903b55c243610d944c9262
Ronin Pinto <Ronin Pinto>
2016-06-04 20:43:10 -0700

    תיקון המערכת האוטומטית הוספת קישוריות למחלקת המייל הוספת קוד HTML אינטגרצית המייל בקוד

--- Ghealth/src/Server/Automation.java
+++ Ghealth/src/Server/Automation.java
@@ -14,6 +14,8 @@ import java.util.GregorianCalendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.mail.MessagingException;
+
 import enums.DoctorSpeciallity;
 import enums.Status;
 import models.AppointmentSettings;
@@ -23,6 +25,7 @@ import models.Patient;
 
 public class Automation extends Thread{
 	private Timer timer = new Timer();
+	//public static Email mail= new Email();
 	public static List<Notification> notLst = new ArrayList<Notification>();
 	public static ResultSet result = null;
 	public Statement stmt; 
@@ -33,11 +36,22 @@ public class Automation extends Thread{
 	Calendar cal = new GregorianCalendar();
 	
 	public void run(){
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			/** 
+			 * here we can enter a starting date to schedule mailing at a certain hour
+			 * if there isn't a future date it will start immediately and repeat with the given value
+			 */
+			Date startDate = dateFormatter.parse("2016-06-04 19:14:30");
+			timer.schedule(new PeriodicNotification(),startDate, 24 * 60 * 60 * 1000);  //Every 24 hours at 8AM
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		timer.schedule(new PeriodicReport(),0);
-		timer.schedule(new PeriodicNotification(),0);
 	}
 	
-	
+	//-------------------------------------------------------------------------------------
 	public class PeriodicReport extends TimerTask{
 		public void run(){
 			
@@ -46,17 +60,20 @@ public class Automation extends Thread{
 			timer.schedule(new PeriodicReport(), 7 * 24 * 60 * 60 * 1000); // every week
 		}
 	}
-
+	
+	//-------------------------------------------------------------------------------------
 	public class PeriodicNotification extends TimerTask{
 		public void run(){
 			/* TODO Checks for changed status appointment notifications */
 
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 			SimpleDateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
+			SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			Calendar tommorow = Calendar.getInstance();
 			tommorow.add(Calendar.DATE, 1);
 			String tommorow_b = formatter.format(tommorow.getTime());
-			
+
+			//-------------------------------------------------------------------------------------
 			
 			querystr = "SELECT  apsID,apsPtID,apsDate,apsTime,apsCreateDate,apsCreateTime,apsStatus,apsDocID,uFirstName,uLastName,cID,cName,cLocation,dSpeciality,ptEmail "
 					+ "FROM appointmentsettings,user,clinic,doctor,patient "
@@ -70,47 +87,75 @@ public class Automation extends Thread{
 			try {
 				while (result.next())
 				{
+					/* -------------parsing tuple--------------- */ 
+					System.out.println("PASS THROUGH TUPLE");
 					Status st =  Status.valueOf(result.getString(7));
 					as = new AppointmentSettings(result.getInt(1),result.getString(2),result.getString(3),result.getString(4),
-							result.getString(5),result.getString(6),st,result.getString("apsDocID"));
+					result.getString(5),result.getString(6),st,result.getString("apsDocID"));
 					Patient pt= new Patient();
 					pt.setPtEmail(result.getString("ptEmail"));
 					Clinic clinic = new Clinic(result.getInt("cID"),result.getString("cName"),result.getString("cLocation"));
 					DoctorSpeciallity ds = DoctorSpeciallity.valueOf(result.getString("dSpeciality"));
 					doctor = new Doctor(result.getString("apsDocID"),result.getString("uFirstName"),result.getString("uLastName"),clinic,ds);
 					as.setDoctor(doctor);
-					/* prep notification obj */
+					/* -------------end of parsing--------------- */ 
+					
+					/* Preparing notification object */
 					Notification nt = new Notification();
 					Date dt = formatter.parse(as.getApsDate());
 					nt.date = dt;
-					//nt.date=as.getApsDate();
 					nt.docName="Dr. " + doctor.getuLastName() + " " + doctor.getuFirstName();
 					nt.location=clinic.getcLocation();
 					nt.mail=pt.getPtEmail();
 					System.out.println(nt.date + " "+ nt.docName+" "+nt.mail+ " "+nt.location+ " ");
+					/* Preparing notification object - end */
+					
+					//-------------------------------------------------------------------------------------
 					/* checking if this is todays notification */
-					Date today = c.getTime();
+					/*
+					//c.getTime();
 					cal.setTime(dt);
 					int diffrence = cal.get(Calendar.DAY_OF_MONTH)-c.get(Calendar.DAY_OF_MONTH);
 					System.out.println("diff: "+diffrence);
 					if(diffrence <= 1 && diffrence >=0)
-						sendMail(nt);
+					*/
+					//-------------------------------------------------------------------------------------
+					
+					/** Sending mail **/
+					sendMail(nt);
+					/** Sending mail **/
 				}
 			} catch (SQLException | ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			/* -------------end of parsing--------------- */ 
-
-			timer.schedule(new PeriodicNotification(), 5 * 1000); // Suppose to be every hour but now its every 15 seconds for us to notice in testing
+			
+			//-------------------------------------------------------------------------------------
+			/*  trash that we'll maybe need later
+			Date startDate;
+			try {
+				startDate = dateFormatter.parse("2016-06-04 19:14:30");
+			//	timer.schedule(new PeriodicNotification(), startDate); // Every 24 hours at 8AM
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/
+			//-------------------------------------------------------------------------------------
 		}
 		
 		private void sendMail(Notification nt) {
 			// TODO Auto-generated method stub
-			
-			
+			try {
+				Email.generateAndSendEmail(nt);
+				System.out.println("\n\n ===> The System has just sent an Email successfully. Check your email..");
+			} catch (MessagingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-
+		
+		//-------------------------------------------------------------------------------------
 		public void getSql(String query){
 		//	Statement stmt;
 			try {
@@ -123,7 +168,7 @@ public class Automation extends Thread{
 			}
 		}
 		
-		
+		//-------------------------------------------------------------------------------------
 		public boolean searchUserSession(String mail){
 	    	for(Notification notf: notLst) {
 	    	    if(notf.mail.equals(mail))
