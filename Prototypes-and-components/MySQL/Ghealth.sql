d22589096b683ede7504a4d93a754f0e7d5bd10c
Amir Shtaiman <Amir Shtaiman>
2016-06-06 15:02:39 +0300

    SQL update for doctor record

--- Prototypes-and-components/MySQL/Ghealth.sql
+++ Prototypes-and-components/MySQL/Ghealth.sql
@@ -16,6 +16,26 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Temporary view structure for view `amir`
+--
+
+DROP TABLE IF EXISTS `amir`;
+/*!50001 DROP VIEW IF EXISTS `amir`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `amir` AS SELECT 
+ 1 AS `apsID`,
+ 1 AS `apsPtID`,
+ 1 AS `apsDate`,
+ 1 AS `apsTime`,
+ 1 AS `apsCreateDate`,
+ 1 AS `apsCreateTime`,
+ 1 AS `apsStatus`,
+ 1 AS `apsDocID`,
+ 1 AS `apsSummery`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `appointmentsettings`
 --
 
@@ -31,12 +51,13 @@ CREATE TABLE `appointmentsettings` (
   `apsCreateTime` time DEFAULT NULL,
   `apsStatus` varchar(45) DEFAULT NULL,
   `apsDocID` varchar(45) DEFAULT NULL,
+  `apsSummery` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`apsID`),
   KEY `apsPtID_idx` (`apsPtID`),
   KEY `apsDocID_idx` (`apsDocID`),
   CONSTRAINT `apsDocID` FOREIGN KEY (`apsDocID`) REFERENCES `user` (`uID`) ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT `apsPtID` FOREIGN KEY (`apsPtID`) REFERENCES `patient` (`ptID`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +66,7 @@ CREATE TABLE `appointmentsettings` (
 
 LOCK TABLES `appointmentsettings` WRITE;
 /*!40000 ALTER TABLE `appointmentsettings` DISABLE KEYS */;
-INSERT INTO `appointmentsettings` VALUES (1,'4444','2016-07-15','10:30:00','2016-06-15','14:01:43','ARRIVED','5001'),(2,'4444','2016-08-15','11:00:00','2016-06-15','14:01:43','ARRIVED','5004'),(3,'4444','2016-09-15','11:30:00','2016-06-15','14:01:43','ARRIVED','5005'),(4,'4000','2016-07-15','12:00:00','2016-06-15','14:01:43','ARRIVED','5002'),(5,'4000','2016-08-15','12:30:00','2016-06-15','14:01:43','ARRIVED','5003'),(6,'4003','2016-07-15','13:00:00','2016-06-15','14:01:43','ARRIVED','5002'),(7,'4003','2016-08-15','13:30:00','2016-06-15','14:01:43','ARRIVED','5003'),(8,'4444','2016-08-15','08:00:00','2016-08-15','15:30:00','ARRIVED','5001'),(9,'4444','2016-07-15','08:00:00','2016-07-15','08:00:00','ARRIVED','5002'),(10,'4444','2016-06-15','13:00:00','2016-07-15','08:00:00','ARRIVED','5004'),(11,'4444','2016-09-15','13:00:00','2016-07-15','08:00:00','SCHEDUELD','5011'),(12,'4444','2016-06-24','09:30:00','2016-06-03','00:18:56','SCHEDUELD','5000'),(13,'4444','2016-06-17','14:30:00','2016-06-03','00:27:19','SCHEDUELD','5014');
+INSERT INTO `appointmentsettings` VALUES (1,'4444','2016-07-15','10:30:00','2016-06-15','14:01:43','ARRIVED','5001',NULL),(2,'4444','2016-08-15','11:00:00','2016-06-15','14:01:43','ARRIVED','5004',NULL),(3,'4444','2016-09-15','11:30:00','2016-06-15','14:01:43','ARRIVED','5005',NULL),(4,'4000','2016-07-15','12:00:00','2016-06-15','14:01:43','ARRIVED','5002',NULL),(5,'4000','2016-08-15','12:30:00','2016-06-15','14:01:43','ARRIVED','5003',NULL),(6,'4003','2016-07-15','13:00:00','2016-06-15','14:01:43','ARRIVED','5002',NULL),(7,'4003','2016-08-15','13:30:00','2016-06-15','14:01:43','ARRIVED','5003',NULL),(8,'4444','2016-08-15','08:00:00','2016-08-15','15:30:00','ARRIVED','5001',NULL),(9,'4444','2016-07-15','08:00:00','2016-07-15','08:00:00','ARRIVED','5002',NULL),(10,'4444','2016-06-15','13:00:00','2016-07-15','08:00:00','ARRIVED','5004',NULL),(11,'4444','2016-09-15','13:00:00','2016-07-15','08:00:00','CANCELED','5011',NULL),(12,'4444','2016-06-24','09:30:00','2016-06-03','00:18:56','CANCELED','5000',NULL),(13,'4444','2016-06-17','14:30:00','2016-06-03','00:27:19','CANCELED','5014',NULL),(14,'4444','2016-06-16','09:30:00','2016-06-04','15:13:31','CANCELED','5010',NULL),(15,'4444','2016-06-06','10:30:00','2016-06-04','15:56:46','CANCELED','5009',NULL),(16,'4444','2016-06-05','10:00:00','2016-06-04','15:57:49','CANCELED','5014',NULL),(17,'4444','2016-06-22','11:00:00','2016-06-06','14:59:25','SCHEDUELD','5000',NULL),(18,'4444','2016-06-30','16:00:00','2016-06-06','15:00:20','SCHEDUELD','5006',NULL);
 /*!40000 ALTER TABLE `appointmentsettings` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -249,6 +270,24 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES ('4000','1234','Amir','Sht','amir@gm.com',1,'CUSTOMER_SERVICE'),('5000','1234','Moshe','Pinto','Pinto@gmail.com',1,'DOCTOR'),('5001','1234','Eran','Tamir','Eran.Tamir@walla.co.il',2,'DOCTOR'),('5002','1234','Yossi','Leitner','Yossi.Leitner@walla.co.il',3,'DOCTOR'),('5003','1234','Hossam','Ra\'d','Hossam.Rara@walla.com',4,'DOCTOR'),('5004','1234','Viktor','Klein','ViktorVikKlein@yahoo.com',5,'DOCTOR'),('5005','1234','Slava','Babayev','SlavSlavaBBB@gmail.com',6,'DOCTOR'),('5006','1234','Vitaliy','Gitman','Vital@iol.com',7,'DOCTOR'),('5007','1234','Alexei','Yad-Shalom','Alexei@yahoo.coom',1,'DOCTOR'),('5008','1234','Vladimir','Ruhman','Vladimirruhruh@yahoo.com',2,'DOCTOR'),('5009','1234','Guy','Reznik','guyrez@walla.com',3,'DOCTOR'),('5010','1234','Guri','Alfi','AlfiGurgur@gmail.com',4,'DOCTOR'),('5011','1234','Ovadia','Cohen','ovad@gmail.com',5,'DOCTOR'),('5012','1234','Ilya','Vered','ilyaver@gmail.com',6,'DOCTOR'),('5013','1234','Shasha','Stoyko','Stoykoshasha@gmail.com',7,'DOCTOR'),('5014','1234','Sveta','Aronov','SvetaRonova@gmail.com',5,'DOCTOR'),('5015','1234','Irena','Glushko','Irenaglushko@gmail.com',2,'DOCTOR');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `amir`
+--
+
+/*!50001 DROP VIEW IF EXISTS `amir`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `amir` AS select `a`.`apsID` AS `apsID`,`a`.`apsPtID` AS `apsPtID`,`a`.`apsDate` AS `apsDate`,`a`.`apsTime` AS `apsTime`,`a`.`apsCreateDate` AS `apsCreateDate`,`a`.`apsCreateTime` AS `apsCreateTime`,`a`.`apsStatus` AS `apsStatus`,`a`.`apsDocID` AS `apsDocID`,`a`.`apsSummery` AS `apsSummery` from `appointmentsettings` `a` where ((`a`.`apsPtID` = '4444') and (`a`.`apsStatus` = 'ARRIVED')) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -259,4 +298,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-03  2:38:49
+-- Dump completed on 2016-06-06 15:01:57
