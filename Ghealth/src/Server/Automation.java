632525c7f70a8ed565bb958982920ddd70a8c7c0
mey <mey@visualead.com>
2016-06-10 15:04:46 +0300

    weeklyReports is now singleton designed

--- Ghealth/src/Server/Automation.java
+++ Ghealth/src/Server/Automation.java
@@ -64,8 +64,8 @@ public class Automation extends Thread{
 		public void run(){
 			
 			/* TODO Creating weekly report */
-			WeeklyReports rep = new WeeklyReports();
-			rep.createAllWeeklyReports();
+			WeeklyReports rep = WeeklyReports.getInstance();
+			rep.createAllClinicsWeeklyReports();
 			
 			timer.schedule(new PeriodicReport(), 24 * 60 * 60 * 1000); // every day for the past week
 		}
