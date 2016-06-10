af2aca1e9a29bcd18d7b535e22739b8af6fbf1f7
mey <mey@visualead.com>
2016-06-10 15:07:58 +0300

    changed - sql query calculate min/avg/max/sd also all reports are saved in a list and passed to client also as a list

--- Ghealth/src/Server/WeeklyReports.java
+++ Ghealth/src/Server/WeeklyReports.java
@@ -1,5 +1,6 @@
 package Server;
 
+import java.awt.Component;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,6 +14,10 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+import client.ShowWeeklyReports;
 import enums.DoctorSpeciallity;
 import enums.Status;
 import enums.task;
@@ -21,24 +26,54 @@ import models.Clinic;
 import models.Doctor;
 import models.Envelope;
 
+
+/*
+ *  private static ClassicSingleton instance = null;
+   private ClassicSingleton() {
+      // Exists only to defeat instantiation.
+   }
+   public static ClassicSingleton getInstance() {
+      if(instance == null) {
+         instance = new ClassicSingleton();
+      }
+      return instance;
+   }*/
+
+
+
+
 public class WeeklyReports {
 
-	private String [][] sevenDaysData = new String[7][4];
+	
+	
+	private static WeeklyReports instance = null;
+	private List<Object> allReports;
 
-
-	public void WeeklyReports(){
-		
+	
+	
+	private WeeklyReports(){	
+		// Exists only to defeat instantiation.
+		System.out.println("in WeeklyReports constructor");
 	}
 	
 	
+	 public static WeeklyReports getInstance() {
+	      if(instance == null) {
+	         instance = new WeeklyReports();
+	      }
+	      return instance;
+	   }
 	
+	 
+	 
 	//for automation 
-	public void createAllWeeklyReports(){
+	public void createAllClinicsWeeklyReports(){
 	
+		this.allReports = new ArrayList<Object>();
 		ResultSet result = null;
 		Statement stmt;
-		String querystr = "SELECT C.cID FROM clinic C";
-		Integer cID;
+		String querystr = "SELECT C.cID FROM clinic C"; //all clinics 
+		int cID;
 		
 		try {
 			mysqlConnection ms = new mysqlConnection();
@@ -50,8 +85,8 @@ public class WeeklyReports {
 			while(result.next())
 			{
 				cID = result.getInt(1);
-				createReport(cID.intValue());
-				reportToFile("clinic_" + cID.toString());
+				createReport(cID);
+				
 			}
 			ms.conn.close();
 		} catch (SQLException e) {
@@ -59,62 +94,77 @@ public class WeeklyReports {
 			e.printStackTrace();
 		}
 		
+			
 		
-		
-		
-		
-	} 
+	} //end of createAllWeeklyReports
 	
 	
-	private void createReport(Integer clinicID){
+	private void createReport(int clinicID){
 		
+		List<String[]> weeklyData = new ArrayList<String[]>();
 		ResultSet result = null;
 		Statement stmt;
-		
-		
-		
-		String query_1 = ""
-				+ "SELECT A.apsDate, count(distinct A.apsPtID) as NumOfPatients, AVG(DATEDIFF(A.apsDate,A.apsCreateDate)) as AvgProcessTime, "
-				+ "AVG(timediff(A.apsStartTime, A.apsTime)/60) as AvgWaitingTime "
-				+ " " ;
-						
-		
-		String query_3 = " "
-				+ "FROM Ghealth.appointmentsettings A "
-				+ " "
-				+ "WHERE apsStatus='ARRIVED' and apsDate = '";
-		String query_4 ="' ";//AND A.apsDocID in (SELECT doc.uID FROM user doc WHERE doc.ucID ='";
-		String query_5 = "')";
-		
 		
 
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 		Calendar today = Calendar.getInstance();
 		
-		
+		String to_date_str = formatter.format(today.getTime());
+		today.add(Calendar.DATE, -7);
+		String from_date_str = formatter.format(today.getTime());
 
+
+		String query = "CREATE OR REPLACE VIEW TABLEWEEK AS "
+				+ "SELECT  A.apsDate, "
+				+ "		AVG(DATEDIFF(A.apsDate,A.apsCreateDate)) as AvgProcessTime, "
+				+ "		AVG(timediff(A.apsStartTime, A.apsTime)/60) as AvgWaitingTime, "
+				+ "        Count(DISTINCT A.apsptid) AS NumOfPatients "
+				+ "FROM   appointmentsettings A "
+				+ "WHERE   A.apsstatus = 'ARRIVED' "
+				+ "       AND A.apsDate <= '" + to_date_str 
+				+ "' AND A.apsDate >= '" + from_date_str
+				+ "' "
+				+ "       AND A.apsDocID in (SELECT doc.uID FROM user doc WHERE doc.ucID='" + clinicID
+				+ "') "
+				+ "GROUP BY A.apsDate; ";
+		
+		String query2 = ""
+				+ "SELECT * FROM TABLEWEEK "
+				+ "UNION "
+				+ "SELECT 'Min' as Op, "
+				+ "MIN(AvgProcessTime) as AvgProcessTime,MIN(AvgWaitingTime) as AvgWaitingTime,Min(NumOfPatients) as NumOfPatients "
+				+ " FROM TABLEWEEK "
+				+ "UNION "
+				+ "SELECT  'Max' as Op, "
+				+ "Max(AvgProcessTime),Max(AvgWaitingTime),Max(NumOfPatients) "
+				+ " FROM TABLEWEEK "
+				+ " UNION "
+				+ " SELECT  'Avg' as Op, "
+				+ " AVG(AvgProcessTime),AVG(AvgWaitingTime),AVG(NumOfPatients) "
+				+ " FROM TABLEWEEK "
+				+ " UNION "
+				+ " SELECT  'SD' as Op, "
+				+ " STDDEV(AvgProcessTime),STDDEV(AvgWaitingTime),STDDEV(NumOfPatients) "
+				+ " FROM TABLEWEEK";
 		
 		try {
 			mysqlConnection ms = new mysqlConnection();
 			stmt = ms.conn.createStatement();
 			
+			stmt.executeUpdate(query);
+			result = stmt.executeQuery(query2);
+			
+			
 			/*-- for each day of the past week -- */
-			for(int i=0; i<7;i++)
+			while(result.next())
 			{
-				/* --get from DB the 4 fields : apsDate, NumOfPatients, AvgProcessTime, AvgWaitingTime ---*/
-				today.add(Calendar.DATE, -1);
-				String day_str = formatter.format(today.getTime());
-				String query = query_1 + query_3 + day_str + query_4 ;//+  clinicID.toString() + query_5  ;
-				System.out.println("THE ONE WE NEED: " + query);
-				result = stmt.executeQuery(query);
-				result.next();
-				/*--Generate matrix with fields  --*/
-				this.sevenDaysData[i-1][0] = result.getString(1);
-				this.sevenDaysData[i-1][1] = result.getString(2);
-				this.sevenDaysData[i-1][2] = result.getString(3);
-				this.sevenDaysData[i-1][3] = result.getString(4);
-				
+				/* --get from DB the 4 fields : apsDate, NumOfPatients, AvgProcessTime, AvgWaitingTime ---*/	
+				weeklyData.add(new String[]{result.getString(1), result.getString(2), result.getString(3), result.getString(4)});	
 			}
+			
+			
+			allReports.add(weeklyData);
+	
 			
 			ms.conn.close();
 		} catch (SQLException e) {
@@ -127,68 +177,50 @@ public class WeeklyReports {
 	
 	
 	
-
-	/*-- insert week into file--*/
-	private void reportToFile(String clinic_nameOfFile){
-		
-		
-		try(  PrintWriter output = new PrintWriter( clinic_nameOfFile +".txt" )  ){
-			
-			for(int i =0; i<7;i++){
-				
-				System.out.println(this.sevenDaysData[i][0]);
-				
-				for(int j=0; j<4; j++ ){
-					
-					output.println(this.sevenDaysData[i][j]);
-				}
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}//end of reportToFile function
-	
-	
-	public static Envelope getClinicWeeklyReport(Integer cID){
+	public Envelope getClinicWeeklyReport(int cID){
 	    
-		Envelope en = new Envelope();
-        BufferedReader in =null;
-        String str;
-        int index =0; 
-        List<String> list = new ArrayList<String>();
+		Envelope en = new Envelope();    
+        List<Object> list = new ArrayList<Object>();
         
-		try {
+		try {	
+			list = (List<Object>) this.allReports.get(cID-1);
 			
-			in = new BufferedReader(new FileReader("clinic_" + cID.toString() +".txt"));
-			
-		} catch (FileNotFoundException e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			
 			e.printStackTrace();
 		    en.setStatus(Status.FAILED_EXCEPTION);
 		}
 		
-		
-        try {
-			while((str = in.readLine()) != null){
-			    list.add(str);
-			}
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-			   en.setStatus(Status.FAILED_EXCEPTION);
-		}
-
-		en.addobjList(list);
+		en.setobjList(list);
 		en.setStatus(Status.EXIST);		
 		en.setType(task.GET_CLINIC_WEEKLY_REPORT);
-		
-		
 		return en;
              
 	}//end of getClinicWeeklyReport
 	
+	
+	 
+ 
+    
+    private static class HeaderRenderer implements TableCellRenderer {
+
+        TableCellRenderer renderer;
+
+        public HeaderRenderer(JTable table) {
+            renderer = table.getTableHeader().getDefaultRenderer();
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(
+            JTable table, Object value, boolean isSelected,
+            boolean hasFocus, int row, int col) {
+            return renderer.getTableCellRendererComponent(
+                table, value, isSelected, hasFocus, row, col);
+        }
+    }
+ 
+   
+    
 	
 }
 
